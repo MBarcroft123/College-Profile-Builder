33b51aac2dee4e873811342dfc644bd8d01505c8
Amy Barcroft <abarcroft@gmail.com>
2016-01-21 12:26:41 -0600

    Hooked up the tableView with protocols and functions. It's 100% good.

--- College Profile Builder #1/ViewController.swift
+++ College Profile Builder #1/ViewController.swift
@@ -8,15 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var tableView: UITableView!
+    
+    var colleges = ["DePaul", "North Western University", "Illinois State University"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colleges.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("myCell", forIndexPath: indexPath)
+        cell.textLabel?.text = colleges[indexPath.row]
+        return cell
     }
 
 
