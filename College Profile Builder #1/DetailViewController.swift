82ded99fbb0d18237a8a9741000f2468aee3f7ec
Amy Barcroft <abarcroft@gmail.com>
2016-01-27 12:19:17 -0600

    Adding a detail view controller and setting it up.

---
+++ College Profile Builder #1/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  College Profile Builder #1
+//
+//  Created by Student on 1/27/16.
+//  Copyright © 2016 The Coolest Guy Around. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
