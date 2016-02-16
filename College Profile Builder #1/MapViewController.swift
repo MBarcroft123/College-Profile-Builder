bf3338531be059f3c92dd92b3ab303df70bfc263
Amy Barcroft <abarcroft@gmail.com>
2016-02-16 12:25:33 -0600

    Set up the MapViewContoller view controller and file. It can now be presented modally.

---
+++ College Profile Builder #1/MapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MapViewController.swift
+//  College Profile Builder #1
+//
+//  Created by Student on 2/16/16.
+//  Copyright © 2016 The Coolest Guy Around. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
